eca128767bcf36e6bafbd7742d18f8c303753826
Akhil <Akhil@VJMacbookPro151.local>
2018-01-24 14:41:17 -0800

    wins and losses counters working

--- game.js
+++ game.js
@@ -1,44 +1,109 @@
 
 
-var random;
-var min;
-var max;
-var number;
-function getRandomInt(min, max) {
-  random =  Math.floor(Math.random() * (max - min + 1) + min);
-  console.log(random);
+
+$(document).ready(function () {
+
+  var target;
+  var min;
+  var max;
+  var crstal1;
+  var crstal2;
+  var crstal3;
+  var crstal4;
+  var score = 0;
+  var wins = 0;
+  var losses = 0;
+
+function startGame(){
+  getRandomTarget(19,120);
+  addTarget();
+  refreshCystals();
+  updatingScore();
+  clickGems();
 }
 
-getRandomInt(10,20);
+startGame();
+
+function getRandomTarget(min, max) {
+  target =  Math.floor(Math.random() * (max - min + 1) + min);
+  console.log(target);
+}
 
 
-$(document).ready(function () 
-{
-	$("#random-number").append(random);
+
+function addTarget() {
+	$("#random-number").html(target);
 	$("#random-number").css({"text-align": "center" , "font-size": "500%"});
+}
+
 
 	var number;
 	var total = 0;
 	
-	
-	function randomNumber(min, max) 
-	{
+//genreate a random number for each crystal
+//increment the score each time crystal click by the value of that crystal
+	function generateRandomCrystalNumber() {
   		
-  		number =  Math.floor(Math.random() * (max - min + 1) + min);
-  		console.log(number);
-  		
+  		var number =  Math.floor(Math.random() * (12 - 1 + 1) + 1);
+  		return number;	
 	}
+  function refreshCystals(){
+    
 
-	$(".gems").click(function(){
-        randomNumber(1,5);
-        total = total + number;
-        console.log(total);
-        $("#player-score").html(total);
+      crstal1 = generateRandomCrystalNumber();
+      crstal2=generateRandomCrystalNumber();
+      crstal3 = generateRandomCrystalNumber();
+      crstal4=generateRandomCrystalNumber();
+      
 
+    
+  }
+  function updatingScore(){
+     $("#player-score").html(score);
+  }
+
+
+  function clickGems(){
+    
+    $("#gem1").click(function(){
+      checkGameState()
+
+         score += crstal1;
+         updatingScore();
+      });
+    $("#gem2").click(function(){
+      checkGameState()
+
+         score += crstal2;
+          updatingScore();
+      });
+    $("#gem3").click(function(){
+      checkGameState()
+
+         score += crstal3;
+         updatingScore();
+      });
+    $("#gem4").click(function(){
+      checkGameState()
+         score += crstal4;
+         updatingScore();
 
     });
 
 
+
+}
+function checkGameState() {
+  if (score === target) {
+    wins++;
+    $("#win-count").text(wins);
+    startGame()
+  } else if(score > target){
+    losses++;
+    $("#loss-count").text(losses);
+    startGame();
+  }
+}
    // total = total + number;
 
     //console.log(total);
